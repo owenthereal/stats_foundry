e18660b9788c4fa8271ef67895110a855e741e79
Jingwen Owen Ou <jingweno@gmail.com>
2011-09-10 22:38:57 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatsFoundry::Application.config.secret_token = '5bad09c0b970dd8b380d106be30e5b0959debf8d6b21ea0c7b7fb7dbdf653677bf7659c3a498d2cdd4ef28aee80ecd0fd5ef878418046119f9c118486ebae39c'
