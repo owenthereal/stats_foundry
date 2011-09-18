259ad43106b3c15daf2d35cc9bdea3eace183043
Jingwen Owen Ou <jingweno@gmail.com>
2011-09-17 20:55:44 -0700

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 user = User.create!(:email => "owen.ou@statsfoundry.com", :password => "111111")
 table = Table.create!(:user => user, :name => "StatsFoundary Analytics")
 { "visited_at" => DateTimeColumn, "url" => TextColumn, "ip" => TextColumn }.each do |column_name, column_type|
-  column_type.create!(:type => column_type, :table => table, :name => column_name)
+  column_type.create!(:type => column_type.name, :table => table, :name => column_name)
 end
