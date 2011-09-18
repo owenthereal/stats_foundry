8fe1a4dcdb0cfbcfe97e5869522b5b488411d13e
Jingwen Owen Ou <jingweno@gmail.com>
2011-09-17 21:17:15 -0700

    Change seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 user = User.create!(:email => "owen.ou@statsfoundry.com", :password => "111111")
 table = Table.create!(:user => user, :name => "StatsFoundary Analytics")
-{ "visited_at" => DateTimeColumn, "url" => TextColumn, "ip" => TextColumn }.each do |column_name, column_type|
-  column_type.create!(:type => column_type.name, :table => table, :name => column_name)
+{ "visited_at" => "DateTimeColumn", "url" => "TextColumn", "ip" => "TextColumn" }.each do |column_name, column_type|
+  column = Column.new(:table => table, :name => column_name)
+  column.type = column_type
+  column.save!
 end
